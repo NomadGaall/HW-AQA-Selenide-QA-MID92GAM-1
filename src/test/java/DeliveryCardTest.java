37ae4351af4524508a037f2dea5b68f13825d091
NomadGaall <167159350+NomadGaall@users.noreply.github.com>
2024-10-24 14:38:46 +0500

    Deleting a check

--- src/test/java/DeliveryCardTest.java
+++ src/test/java/DeliveryCardTest.java
@@ -25,9 +25,6 @@ public class DeliveryCardTest {
         $("[data-test-id='phone'] input").setValue("+79012345577");
         $("[data-test-id=agreement]").click();
         $$("button").find(Condition.exactText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(Condition.hidden, Duration.ofSeconds(15));
-        $(withText("Встреча успешно забронирована")).shouldBe(Condition.hidden, Duration.ofSeconds(15));
-
 
         $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
         $("[data-test-id=notification]").shouldHave(Condition.text("Успешно!\n" + "Встреча успешно забронирована на " + dateGenerate(5, "dd.MM.yyyy"))).shouldBe(Condition.visible);
